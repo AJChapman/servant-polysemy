d16b6103f26b3d6c16303511d8e60fbea28fd459
Lana Black <lana@amok.cc>
2022-11-04 17:04:22 +0000

    Update generic server example.

--- example/ServerGeneric.hs
+++ example/ServerGeneric.hs
@@ -6,14 +6,15 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Main (main) where
 
-import Control.Lens.Operators
+import Data.Function ((&))
 import Data.Version (Version, showVersion)
 import Paths_servant_polysemy as Paths
 import Polysemy
 import Polysemy.Error
 import Servant
+import Servant.API.Generic
 import Servant.Polysemy.Server
-import Servant.API.Generic (ToServantApi, (:-), Generic)
+import Servant.Server.Generic
 
 {-
 This example is of the same server as defined in Server.hs, but it uses Servant.API.Generic to define the path.
@@ -27,17 +28,21 @@ data Routes route = Routes
 -- This is the endpoint, which responds to a GET at /api/v1/version with the server's version in JSON format.
 type MyApi = ToServantApi Routes
 
--- This is the implementation of the API.
--- It also does some IO to print to the terminal.
-myServer :: Member (Embed IO) r => ServerT MyApi (Sem (Error ServerError ': r))
-myServer = do
-  embed $ putStrLn $ "Returning version " <> showVersion Paths.version
+-- The handler for the route
+versionHandler :: Members [Embed IO, Error ServerError] r => Sem r Version
+versionHandler = do
+  embed (putStrLn $ "Returning version " <> showVersion Paths.version)
   pure Paths.version
+
+routes :: Members [Embed IO, Error ServerError] r => Routes (AsServerT (Sem r))
+routes = Routes { _version = versionHandler }
+
+myServer :: Member (Embed IO) r => ServerT MyApi (Sem (Error ServerError ': r))
+myServer = toServant routes
 
 -- This runs Warp (a Haskell web server), serving up our API.
 main :: IO ()
-main =
-  runWarpServer @MyApi 8080 True myServer
-    & runM
+main = runWarpServer @MyApi 8080 True myServer
+     & runM
 
 
